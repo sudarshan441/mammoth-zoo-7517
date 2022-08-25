ada8360332049379926836c5ab6a50a26248ddf0
Santoshdandin <santusb427@gmail.com>
2022-08-25 22:25:14 +0530

    added day-3 file

---
+++ film.js
@@ -0,0 +1,128 @@
+let shortsArray = [
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/876/068/e86b68eacc49f7345ff8cd705689da9e_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1657136595&auto=format&frame=1&q=92&s=b7f9f30acf3fb2a01de598ab56d447c8',
+        title:'S.T.A.L.K.E.R. | Shadow of the Zone',
+        description:'A live-action sci-fi short fan-film based on the S.T.A.L.K.E.R. video-game series.',
+        director:'By Stephen Hancock'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/827/395/fb1ae1e9927b24b7afec6191a46a9741_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1656708049&auto=format&frame=1&q=92&s=c1a18e54d05b2221516cf802e1855ef2',
+        title:'Impression',
+        description:'As a young artist is having doubts about her engagement, her outlook on life shifts after a day spent with a free-spirited young woman.',
+        director:'By Brandi Stevens'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/038/185/655/815a0bc8a0aeccc894cd3e17306be9a7_original.jpg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1659705083&auto=format&frame=1&q=92&s=d8d929ec4b870006cb268e2a3e77bbde',
+        title:'A Matter Of Choice',
+        description:'A dark romantic comedy in an assisted suicide clinic where the happy ever after, might not be so happy.',
+        director:'By Louis'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/038/017/409/403855f86f3f0495df0c0b4a85993713_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1658325924&auto=format&frame=1&q=92&s=e0c77d9b0322512f942bb786e2b13c8a',
+        title:'Morris Code',
+        description:'A Romantic Dramedy',
+        director:'By Rhett Rossi'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/946/341/d9d2b3ae46060712a467d5b6b5020938_original.jpg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1657732933&auto=format&frame=1&q=92&s=d49c5d5b33bf4f61027eae9d4e11ba99',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/817/065/b5c05a20266ca0824caaacffc6dc2f2f_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1656631316&auto=format&frame=1&q=92&s=cd19a1d5f29fd9627050ddcbec75338d',
+        title:'Grace: A Short Film by Natalie Jasmine Harris',
+        description:'1957, USA. Every summer, Grace and her sisters visit their grandparents down south. Now that she’s 13, this summer is unlike any other.',
+        director:'By Natalie Jasmine Harris'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/855/310/00d33fb882de06e2ead187e0e0795af8_original.jpg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1657001972&auto=format&frame=1&q=92&s=29a7f8f7c83af02a356b1d7f2813f788',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/038/008/496/886f2d27658ea071b75eda9eaf612695_original.jpg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1658257995&auto=format&frame=1&q=92&s=8bdbd38a0f675adeb3b1c28f1eab3398',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/996/679/b788e898f95d50370418dd137c44ca0a_original.jpg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1658174825&auto=format&frame=1&q=92&s=f3f23550ef5b4091b433d67d3e7adc79',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/671/943/b214fa798f00dbf5e968bd05796c89bb_original.jpeg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1655420447&auto=format&frame=1&q=92&s=5cc720219a6e104360c730fdfa448017',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/671/943/b214fa798f00dbf5e968bd05796c89bb_original.jpeg?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1655420447&auto=format&frame=1&q=92&s=5cc720219a6e104360c730fdfa448017',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/463/877/1496ecfca322e389dc28fb980cf3f6c4_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1653724065&auto=format&frame=1&q=92&s=ff98b23cf7cb7a87c402fb702f9a4e4c',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+    {
+        image:'https://ksr-ugc.imgix.net/assets/037/427/426/2951aa664f97eb1ef026f543269608d2_original.png?ixlib=rb-4.0.2&crop=faces&w=352&h=198&fit=crop&v=1653461584&auto=format&frame=1&q=92&s=c37f6646cac7183fa371dca00c797cac',
+        title:'PLACES WE KNEW - A Japanese American Short Film',
+        description:'A young American visiting his hometown in Japan must choose between an idyllic vacation or staying to grieve with estranged family.',
+        director:'By Oliver William Staton'
+    },
+
+]
+
+let shortsContainer = document.querySelector('.swiper-wrapper')
+
+displayMovies(shortsArray,shortsContainer)
+
+function displayMovies(data,container){
+    data.forEach((el) => {
+
+        let div = document.createElement('div')
+        div.className = "swiper-slide"
+
+        let image = document.createElement('img')
+        image.src= el.image;
+
+        let title = document.createElement('h3')
+        title.innerText=el.title
+
+        let description = document.createElement('p')
+        description.innerText=el.description
+
+        let director = document.createElement('span');director.innerText=el.director
+
+
+div.append(image,title,description,director)
+
+        container.append(div)
+
+        
+    });
+}
+
+
+
+
